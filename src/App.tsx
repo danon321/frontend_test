762085b897acb94a94e6376f8499e316ee7ffac2
Daniel <44358467+danon321@users.noreply.github.com>
2024-07-11 21:46:15 +0200

    Add content and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import "./App.scss";
+import "./styles/layout/_section.scss";
+import "./styles/components/_text.scss";
+
 import Button from "./components/Button";
 import InputRadio from "./components/InputRadio";
 import Footer from "./layout/Footer";
@@ -11,19 +14,23 @@ function App() {
       <main className="home">
         <h1 className="home__title title title--underline">Nagłówek H1</h1>
         <div className="home__blocks">
-          <section>
-            <h2>Blok 1</h2>
+          <section className="section home__options">
+            <h2 className="section__title">Blok pierwszy</h2>
             <InputRadio>Opcja pierwsza</InputRadio>
             <InputRadio>Opcja druga</InputRadio>
             <InputRadio>Opcja losowa</InputRadio>
           </section>
-          <section>
-            <h2>Blok 2</h2>
-            <Button>Zastąp</Button>
-            <Button>Doklej</Button>
+          <section className="section home__actions">
+            <h2 className="section__title">Blok drugi</h2>
+            <div className="home__buttons">
+              <Button>Zastąp</Button>
+              <Button>Doklej</Button>
+            </div>
           </section>
-          <section>
-            <h2>Blok 3</h2>
+          <section className="section home__content">
+            <h2 className="section__title title title--cutoff">
+              Blok z długą nazwą która sama sie przytnie asd
+            </h2>
             <p className="desc">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
