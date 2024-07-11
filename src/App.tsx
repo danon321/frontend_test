963e8b0e7d6cf61f8420cd76fd48c36b649a33b8
Daniel <44358467+danon321@users.noreply.github.com>
2024-07-11 09:37:40 +0200

    Add buttons, improve semantics

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.scss";
+import Button from "./components/Button";
 import Footer from "./layout/Footer";
 import Header from "./layout/Header";
 
@@ -9,9 +10,17 @@ function App() {
       <main className="home">
         <h1 className="home__title title title--underline">Nagłówek H1</h1>
         <div className="home__blocks">
-          <section>Blok 1</section>
-          <section>Blok 2</section>
-          <section>Blok 3</section>
+          <section>
+            <h2>Blok 1</h2>
+          </section>
+          <section>
+            <h2>Blok 2</h2>
+            <Button>Zastąp</Button>
+            <Button>Doklej</Button>
+          </section>
+          <section>
+            <h2>Blok 3</h2>
+          </section>
         </div>
       </main>
       <Footer />
