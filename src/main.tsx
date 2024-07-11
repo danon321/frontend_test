f9bac8455d2ca268b9be2d8c9e5299706418df91
Daniel <44358467+danon321@users.noreply.github.com>
2024-07-11 20:38:01 +0200

    Add root styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import "./styles/base/_main.scss";
 import "./styles/base/_reset.scss";
 import "./styles/base/_typography.scss";
 
